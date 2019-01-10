bb68130b20455a45b9fdf72a647355bc71813069
Spencer Robertson <spencer.robertson@zirconsoftware.co.uk>
2019-01-10 15:51:04 +0000

    Started work on frontend

--- backend/api.js
+++ backend/api.js
@@ -28,69 +28,97 @@ var reconnectOptions = {
 
 var connectionManager = new stompit.ConnectFailover([connectOptions], reconnectOptions);
 
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
 function requestTrainMovement() {
-  connectionManager.connect(function (error, client, reconnect) {
-   if (error) {
-       console.log("Terminal error, gave up reconnecting");
-       return;
-   }
+  MongoClient.connect(url, function(err, db) {
+    connectionManager.connect(function (error, client, reconnect) {
+     if (error) {
+         console.log("Terminal error, gave up reconnecting");
+         return;
+     }
+     else {
+       console.log("CONNECTED");
+     }
 
-   client.on("error", function (error) {
-       console.log("Connection lost. Reconnecting...");
-       reconnect();
-   });
+     client.on("error", function (error) {
+         console.log("Connection lost. Reconnecting...");
+         reconnect();
+     });
 
-   var headers = {
-       "destination": "/topic/TRAIN_MVT_EK_TOC",           // subscribe for a destination to which messages are sent
-       "activemq.subscriptionName": "somename-train_mvt",   // request a durable subscription - set this to an unique string for each feed
-       "ack": "client-individual"                           // the client will send ACK frames individually for each message processed
-   };
+     var headers = {
+         "destination": "/topic/TRAIN_MVT_EK_TOC",           // subscribe for a destination to which messages are sent
+         "activemq.subscriptionName": "somename-train_mvt",   // request a durable subscription - set this to an unique string for each feed
+         "ack": "client-individual"                           // the client will send ACK frames individually for each message processed
+     };
 
-   client.subscribe(headers, function (error, message) {
-       if (error) {
-           console.log("Subscription failed:", error.message);
-           return;
-       }
-       message.readString("utf-8", function (error, body) {
-           if (error) {
-               console.log("Failed to read a message", error);
-               return;
-           }
-           if (body) {
-               var data;
-               try {
-                   data = JSON.parse(body);
-               } catch (e) {
-                   console.log("Failed to parse JSON", e);
-                   return;
-               }
-               async.each(data,
-                   function(item, next) {
-                       // Look for Train Activation messages (msg_type 0001)
-                       if (item.header && item.header.msg_type == "0001") {
-                           console.log(
-                               "Train",
-                               item.body.train_id,
-                               "activated at stanox",
-                               item.body.tp_origin_stanox ? item.body.tp_origin_stanox : item.body.sched_origin_stanox
-                           );
-                       }
-                       next();
-                   }
-               );
-           }
-           client.ack(message); // Send ACK frame to server
+     client.subscribe(headers, function (error, message) {
+         if (error) {
+             console.log("Subscription failed:", error.message);
+             return;
+         }
+         message.readString("utf-8", function (error, body) {
+             if (error) {
+                 console.log("Failed to read a message", error);
+                 return;
+             }
+             if (body) {
+                 var data;
+                 try {
+                     data = JSON.parse(body);
+                 } catch (e) {
+                     console.log("Failed to parse JSON", e);
+                     return;
+                 }
+                 async.each(data,
+                     function(item, next) {
+                         console.log(item);
+                         // Look for Train Activation messages (msg_type 0001)
+                         if (item.header && item.header.msg_type == "0003") {
+
+                              if (err) throw err;
+                              var dbo = db.db("TrainMovement");
+                              var myquery = { train_descr: item.body.train_descr };
+                              var newvalues = { $set: {first_stanox: "Mickey", second_stanox: "Canyon 123" } };
+                              dbo.collection("trains").update(myquery, newvalues, function(err, res) {
+                                if (err) throw err;
+                                console.log("1 document inserted");
+                              });
+
+                             console.log(
+                                 "Train",
+                                 item.body.train_id,
+                                 "activated at stanox",
+                                 item.body.tp_origin_stanox ? item.body.tp_origin_stanox : item.body.sched_origin_stanox
+                             );
+                         }
+                         next();
+                     }
+                 );
+             }
+             client.ack(message); // Send ACK frame to server
+         });
        });
-   });
-});
+     });
+  });
+}
+
+function getListOfTrains() {
+
+
 }
 
 router.get("/listOfTrains", (req, res) => {
-  // requestTrainMovement(function(val) {
-  //   var json = { 'data': val };
-  //   return res.json({ success: true, data: json });
-  // });
-  requestTrainMovement();
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("TrainMovement");
+    dbo.collection("trains").findOne({}, function(err, result) {
+      if (err) throw err;
+      console.log(result.name);
+      db.close();
+    });
+  });
 });
 
 
---
+++ frontend/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import logo from './static/images/logo.svg';
+import './styles/App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        data : null
+    };
+  }
+
+  componentDidMount() {
+    this.getDataFromDb();
+  }
+
+  getDataFromDb = () => {
+    var url = "/api/listOfTrains";
+    fetch(url)
+      .then(data => data.json())
+      .then(res => this.setState({ data: res.data }))
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
