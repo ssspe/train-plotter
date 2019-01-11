aff6bcefc1a89534e433c51a1033be1586fc946e
Spencer Robertson <spencer.robertson@zirconsoftware.co.uk>
2019-01-11 14:13:42 +0000

    Adding info to the marker click

--- frontend/src/components/MapContainer.js
+++ frontend/src/components/MapContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import ReactMapboxGl, { GeoJSONLayer, Layer, Feature, Marker } from "react-mapbox-gl";
-import Icon from "../static/images/map-marker.png";
+import ReactMapboxGl, { GeoJSONLayer, Layer, Feature, Marker, Popup } from "react-mapbox-gl";
+import Tooltip from '@material-ui/core/Tooltip';
+
 const Map = ReactMapboxGl({
   accessToken: "pk.eyJ1Ijoic3NzcGUiLCJhIjoiY2pxcDNkZWluMDFoazN4dGd6bTY3bnA1ayJ9.9vYYYBBh2scR2shTbCUHFg"
 })
@@ -17,7 +18,9 @@ class MapContainer extends Component {
         data : null,
         trainInfo: null,
         first_coord: null,
-        second_coord: null
+        second_coord: null,
+        open: false,
+        arrival_time: null
     };
   }
 
@@ -29,6 +32,7 @@ class MapContainer extends Component {
       this.getCoordinates(nextProps.trainInfo.second_journey, coord => {
         this.setState({second_coord: coord});
       });
+      this.setState({arrival_time: nextProps.trainInfo.arrival_time})
     }
   }
 
@@ -53,57 +57,58 @@ class MapContainer extends Component {
     });
   }
 
+  handleTooltipOpen = () => {
+    console.log(this.state.open);
+    this.setState({ open: !this.state.open });
+  };
+
   render() {
-    const image = new Image(20, 20);
-    image.src = Icon;
-    const firstImages = ["firstImage", image];
-    const secondImages = ["secondImage", image];
+    console.log(this.props);
     return(
       <Map
         style="mapbox://styles/mapbox/streets-v9"
-        containerStyle={{
+        containerStyle={ {
           height: "100vh",
           width: "100vw",
-          'text-align': "left",
-        }}>
-          {this.state.first_coord ?
-            <Marker
-                coordinates={[this.state.first_coord[0], this.state.first_coord[1]]}
-                onClick={() => {
-                  console.log("Clicked");
-                }}
-              >
-                <img height="20" width="20" src={require("../static/images/map-marker.png")}/>
-              </Marker> : null }
-          {this.state.second_coord ?
-            <Marker
-                coordinates={[this.state.second_coord[0], this.state.second_coord[1]]}
-                onClick={() => {
-                  console.log("Clicked");
-                }}
-              >
-                <img height="20" width="20" src={require("../static/images/map-marker.png")}/>
-              </Marker> : null }
-          {this.state.first_coord && this.state.second_coord ?
+          'text-align': "left" } }>
+        { this.state.first_coord ?
+          <Marker
+            coordinates={ [this.state.first_coord[0], this.state.first_coord[1]] }
+            onClick={ () => { console.log("Clicked"); } }>
+            <img height="20" width="20" src={ require("../static/images/map-marker.png") } />
+          </Marker> : null }
+        { this.state.second_coord ?
+          <Marker
+              coordinates={ [this.state.second_coord[0], this.state.second_coord[1]] }
+              onClick={ this.handleTooltipOpen }>
+            <img height="20" width="20" src={ require("../static/images/map-marker.png") } />
+          </Marker> : null }
+        { this.state.open ?
+          <Popup
+            coordinates={ [this.state.second_coord[0], this.state.second_coord[1]] }
+            offset={{
+              'bottom-left': [12, -38],  'bottom': [0, -38], 'bottom-right': [-12, -38]
+            }}>
+            <h1>{this.state.trainInfo.arrival_time}</h1>
+          </Popup> : null }
+        { this.state.first_coord && this.state.second_coord ?
           <GeoJSONLayer
-             data={{
-               'type': 'FeatureCollection',
-               'features': [
-                 {
-                   'type': 'Feature',
-                   'geometry': {
-                     'type': 'LineString',
-                     'coordinates': [
-                       [this.state.first_coord[0], this.state.first_coord[1]],
-                       [this.state.second_coord[0], this.state.second_coord[1]]
-                     ]
-                   }
-                 }
-               ]
-             }}
-             linePaint={linePaint}
-           /> : null }
-
+            data={ {
+              'type': 'FeatureCollection',
+              'features': [
+                {
+                  'type': 'Feature',
+                  'geometry': {
+                    'type': 'LineString',
+                    'coordinates': [
+                      [this.state.first_coord[0], this.state.first_coord[1]],
+                      [this.state.second_coord[0], this.state.second_coord[1]]
+                    ]
+                  }
+                }
+              ]
+            } }
+            linePaint={linePaint} /> : null }
       </Map>
     );
   }
