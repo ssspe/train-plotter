7612f0c8a387c8002eb1d1f903020e8b212d20e9
Spencer Robertson <spencer.robertson@zirconsoftware.co.uk>
2019-01-17 14:04:38 +0000

    Using react-mapbox-wrapper to add geoJsonLines

--- frontend/src/components/MapContainer.js
+++ frontend/src/components/MapContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import MapboxMap, { Marker, GeoJSONLayer } from 'react-mapbox-wrapper';
+import MapboxMap, { Marker, GeoJSONLayer, Helpers } from 'react-mapbox-wrapper';
+// import { drawGeoJSON } from 'react-mapbox-wrapper/Helpers';
 import _ from 'lodash';
 import Icon from '../static/images/map-marker.png';
 
@@ -22,7 +23,6 @@ class MapContainer extends Component {
     let secondMarkers;
     let firstMarkers;
     const { trainInfo } = this.props;
-    console.log(trainInfo)
     if (this.map) {
       var d = new Date(0)
       d.setUTCSeconds(trainInfo.arrival_time / 1000)
@@ -43,48 +43,36 @@ class MapContainer extends Component {
             popupOnOver
             popupOffset={20} />
 
-      if (this.map.getLayer("route" + trainInfo.train_descriptor)) {
-        this.map.removeLayer("route" + trainInfo.train_descriptor);
-      }
+      var geoJsonData = {"data": {
+                "type": "Feature",
+                "properties": {},
+                "geometry": {
+                    "type": "LineString",
+                    "coordinates": [
+                        trainInfo.current_journey,
+                        trainInfo.second_journey
+                    ]
+                }
+            }};
+      console.log(geoJsonData);
+      var paint = { 'fill-color': 'blue' }
+      Helpers.drawGeoJSON(
+        this.map,
+        trainInfo.train_descriptor,
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'LineString',
+                coordinates: [
+                    trainInfo.current_journey,
+                    trainInfo.second_journey
+                ],
+            },
+        },
+        paint, () => {console.log("DONE")});
 
-      if (this.map.getSource("source" + trainInfo.train_descriptor)){
-        this.map.removeSource("source" + trainInfo.train_descriptor);
-      }
-
-      if (!this.map.getSource("source" + trainInfo.train_descriptor)){
-        this.map.addSource("source" + trainInfo.train_descriptor, {
-          "type": "geojson",
-          "data": {
-              "type": "Feature",
-              "properties": {},
-              "geometry": {
-                  "type": "LineString",
-                  "coordinates": [
-                      trainInfo.current_journey,
-                      trainInfo.second_journey
-                  ]
-              }
-          }
-        });
-      }
-
-      if (!this.map.getLayer("route" + trainInfo.train_descriptor)){
-        this.map.addLayer({
-          "id": "route" + trainInfo.train_descriptor,
-          "type": "line",
-          "source": "source" + trainInfo.train_descriptor,
-          "layout": {
-              "line-join": "round",
-              "line-cap": "round"
-          },
-          "paint": {
-              "line-color": "#888",
-              "line-width": 8
-          }
-        });
-      }
     }
-    
+
     return (
       <div style={{
         height: "100vh",
